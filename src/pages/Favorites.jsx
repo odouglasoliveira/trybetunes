9497b361b02daaf9d199de8eb8ea305488a57037
Douglas Oliveira <doglasolivera13@gmail.com>
2023-03-13 14:54:47 -0300

    Created app routes: requisito 1

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites">
+        Teste
+      </div>
+    );
+  }
+}
+
+export default Favorites;
---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        Teste
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
