f114bf30c4820c23374b22a70167751ff702cdf5
Douglas Oliveira <doglasolivera13@gmail.com>
2023-03-13 19:36:10 -0300

    criação da pasta components

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import Loading from './Loading';
+import { getUser } from '../services/userAPI';
+import './Header.css';
+
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+    };
+  }
+
+  async showUser() {
+    const { name } = await getUser();
+    this.setState({
+      isLoading: false,
+    });
+    return name;
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      isLoading ? (
+        <Loading />
+      ) : (
+        <header data-testid="header-component">
+          <div>
+            Bem vindo
+          </div>
+        </header>
+      )
+    );
+  }
+}
+
+export default Header;
