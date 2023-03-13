7300003b87e9ec284e63f8f3c9e2b256c6ef3fad
Douglas Oliveira <doglasolivera13@gmail.com>
2023-03-13 19:09:37 -0300

    Correção de erros do lint

---
+++ src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './Loading.css';
+
+class Loading extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Carregando...</h1>
+      </div>
+    );
+  }
+}
+
+export default Loading;
