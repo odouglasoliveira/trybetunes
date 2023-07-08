573258ca604480d4c376baa3da38845225442906
Douglas Oliveira <doglasolivera13@gmail.com>
2023-07-08 05:28:35 -0300

    refactor: remoção de loadings desnecessários

--- src/services/favoriteSongsAPI.js
+++ src/services/favoriteSongsAPI.js
@@ -1,6 +1,4 @@
 const FAVORITE_SONGS_KEY = 'favorite_songs';
-const TIMEOUT = 1;
-const SUCCESS_STATUS = 'OK';
 
 if (!JSON.parse(localStorage.getItem(FAVORITE_SONGS_KEY))) {
   localStorage.setItem(FAVORITE_SONGS_KEY, JSON.stringify([]));
@@ -10,34 +8,19 @@ const readFavoriteSongs = () => JSON.parse(localStorage.getItem(FAVORITE_SONGS_K
 const saveFavoriteSongs = (favoriteSongs) => localStorage
   .setItem(FAVORITE_SONGS_KEY, JSON.stringify(favoriteSongs));
 
-// --------------------------------------------------------------------
-// A função simulateRequest simula uma requisição para uma API externa
-// Esse tipo de função que "chama outra função" é chamada de
-// "currying function" https://javascript.info/currying-partials
-// não se preocupe, estudaremos isso futuramente.
-// --------------------------------------------------------------------
-
-const simulateRequest = (response) => (callback) => {
-  setTimeout(() => {
-    callback(response);
-  }, TIMEOUT);
+export const getFavoriteSongs = () => {
+  const favoriteSongs = readFavoriteSongs();
+  return favoriteSongs;
 };
 
-export const getFavoriteSongs = () => new Promise((resolve) => {
-  const favoriteSongs = readFavoriteSongs();
-  simulateRequest(favoriteSongs)(resolve);
-});
-
-export const addSong = (song) => new Promise((resolve) => {
+export const addSong = (song) => {
   if (song) {
     const favoriteSongs = readFavoriteSongs();
     saveFavoriteSongs([...favoriteSongs, song]);
   }
-  simulateRequest(SUCCESS_STATUS)(resolve);
-});
+};
 
-export const removeSong = (song) => new Promise((resolve) => {
+export const removeSong = (song) => {
   const favoriteSongs = readFavoriteSongs();
   saveFavoriteSongs(favoriteSongs.filter((s) => s.trackId !== song.trackId));
-  simulateRequest(SUCCESS_STATUS)(resolve);
-});
+};
--- src/services/userAPI.js
+++ src/services/userAPI.js
@@ -1,34 +1,19 @@
 import defaultProfile from '../images/do-utilizador.png';
 
 const USER_KEY = 'user';
-const TIMEOUT = 0;
-const SUCCESS_STATUS = 'OK';
 
 const readUser = () => JSON.parse(localStorage.getItem(USER_KEY));
 const saveUser = (user) => localStorage.setItem(USER_KEY, JSON.stringify(user));
 
-// --------------------------------------------------------------------
-// A função simulateRequest simula uma requisição para uma API externa
-// Esse tipo de função que "chama outra função" é chamada de
-// "currying function" https://javascript.info/currying-partials
-// não se preocupe, estudaremos isso mais futuramente
-// --------------------------------------------------------------------
-
-const simulateRequest = (response) => (callback) => {
-  setTimeout(() => {
-    callback(response);
-  }, TIMEOUT);
-};
-
-export const getUser = () => new Promise((resolve) => {
-  let user = readUser();
+export const getUser = async () => {
+  let user = await readUser();
   if (user === null) {
     user = {};
   }
-  simulateRequest(user)(resolve);
-});
+  return user;
+};
 
-export const createUser = (user) => new Promise((resolve) => {
+export const createUser = (user) => {
   const emptyUser = {
     name: '',
     email: 'exemplo@email.com',
@@ -36,10 +21,8 @@ export const createUser = (user) => new Promise((resolve) => {
     description: 'Insira sua descrição',
   };
   saveUser({ ...emptyUser, ...user });
-  simulateRequest(SUCCESS_STATUS)(resolve);
-});
+};
 
-export const updateUser = (updatedUser) => new Promise((resolve) => {
+export const updateUser = (updatedUser) => {
   saveUser({ ...updatedUser });
-  simulateRequest(SUCCESS_STATUS)(resolve);
-});
+};
